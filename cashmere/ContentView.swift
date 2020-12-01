8511c069a3e745383db37da6a8e0c74e361acb82
志村豪気 <>
2020-12-01 15:31:39 +0900

    initial commit

---
+++ cashmere/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cashmere
+//
+//  Created by 志村豪気 on 2020/12/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
