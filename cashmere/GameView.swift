1b4743fd94912e0f082d0fee342f201dad38e262
志村豪気 <>
2020-12-02 12:00:08 +0900

    TabBarを追加

--- cashmere/GameView.swift
+++ cashmere/GameView.swift
@@ -9,6 +9,31 @@ import SwiftUI
 
 struct GameView: View {
     var body: some View {
-        MapView()
+        TabView {
+            MapView()
+                .tabItem {
+                    Image(systemName: "1.square.fill")
+                    Text("マップ")
+                }
+            Image(systemName: "faceid")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200.0, height: 200.0)
+                .foregroundColor(.blue)
+                .tabItem {
+                    Image(systemName: "2.square.fill")
+                    Text("アイテム")
+                }
+            Image(systemName: "faceid")
+                .resizable()
+                .scaledToFit()
+                .font(.system(size: 18, weight: .ultraLight, design: .serif))
+                .frame(width: 200.0, height: 200.0)
+                .foregroundColor(.blue)
+                .tabItem {
+                    Image(systemName: "3.square.fill")
+                    Text("プレイヤー")
+                }
+        }
     }
 }
