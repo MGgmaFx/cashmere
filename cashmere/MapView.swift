a2307bc1c95c2b18a9827b93bd5a7f94020b0af5
志村豪気 <>
2020-12-04 15:50:23 +0900

    残り時間を作成

--- cashmere/MapView.swift
+++ cashmere/MapView.swift
@@ -2,13 +2,29 @@ import SwiftUI
 import CoreLocation
 
 struct MapView: View {
+    var toDate = Calendar.current.date(byAdding:.hour,value:1,to:Date())
     @State var manager = CLLocationManager()
     @State var alert = false
     var body: some View {
-        // 以下の行を追加
         // ContentViewに地図を表示
-        mapView(manager: $manager, alert: $alert).alert(isPresented: $alert) {
-          Alert(title: Text("Please Enable Location Access In Setting Panel!!!"))
+        ZStack(alignment: .top) {
+            mapView(manager: $manager, alert: $alert).alert(isPresented: $alert) {
+              Alert(title: Text("Please Enable Location Access In Setting Panel!!!"))
+            }
+            TimerView(setDate: toDate!)
+                .frame(width: 200, height: 60)
+                .background(Color.gray)
+                .foregroundColor(Color.white)
+                .cornerRadius(20)
+                .padding(.horizontal, 20)
+                .padding(.top, 20)
+                
         }
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
     }
 }
---
+++ cashmere/TimerView.swift
@@ -0,0 +1,40 @@
+//
+//  TimerView.swift
+//  cashmere
+//
+//  Created by 志村豪気 on 2020/12/04.
+//
+
+import SwiftUI
+struct TimerView : View {
+  @State var nowD:Date = Date()
+   let setDate:Date
+   
+   var timer: Timer {
+       Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {_ in
+           self.nowD = Date()
+       }
+   }
+   
+   var body: some View {
+    Text(TimerFunc(from: setDate))
+        .font(.title)
+        .onAppear(perform: {
+              _ = self.timer
+        })
+   }
+
+   func TimerFunc(from date:Date)->String{
+       let cal = Calendar(identifier: .japanese)
+
+       let timeVal = cal.dateComponents([.day,.hour,.minute,.second], from: nowD,to: setDate)
+
+       return String(format: "%02d:%02d:%02d:%02d",
+       timeVal.day ?? 00,
+       timeVal.hour ?? 00,
+       timeVal.minute ?? 00,
+       timeVal.second ?? 00)
+       
+   }
+
+}
