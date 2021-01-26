da23d0458d73481cebc493111e7976eb0ae8ca93
志村豪気 <>
2021-01-26 10:10:58 +0900

    fix: 重要な項目を上位に表示するように修正

--- cashmere/Views/GameruleSettingsView.swift
+++ cashmere/Views/GameruleSettingsView.swift
@@ -16,129 +16,131 @@ struct GameruleSettingsView: View {
     @Binding var escapeTime: Int
     @Binding var escapeRange: Int
     var body: some View {
-        VStack {
-            HStack {
-                Text("ルーム名")
-                    .padding(20)
-                    .foregroundColor(.black)
-                
-                Spacer()
-                
-                TextField("ルーム名を入力", text: $room.name)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .foregroundColor(.primary)
-                    .padding()
-            }
-            .padding(.vertical)
-            HStack {
-                Text("制限時間")
-                    .padding(20)
-                    .foregroundColor(.black)
-                
-                Spacer()
-                
-                Picker("", selection: $hour) {
-                    ForEach(0 ..< 100) { num in
-                        Text(String(num) + "時間")
-                            .foregroundColor(.black)
-                    }
+        ScrollView(.vertical) {
+            VStack {
+                HStack {
+                    Text("ルーム名")
+                        .padding(20)
+                        .foregroundColor(.black)
+                    
+                    Spacer()
+                    
+                    TextField("ルーム名を入力", text: $room.name)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .foregroundColor(.primary)
+                        .padding()
                 }
-                .frame(width: 120, height: 60, alignment: .center)
-                .labelsHidden()
-                .compositingGroup()
-                .clipped()
-                
-                Picker("", selection: $minute) {
-                    ForEach(1 ..< 60) { num in
-                        Text(String(num) + "分")
-                            .foregroundColor(.black)
+                .padding(.vertical)
+                HStack {
+                    Text("制限時間")
+                        .padding(20)
+                        .foregroundColor(.black)
+                    
+                    Spacer()
+                    
+                    Picker("", selection: $hour) {
+                        ForEach(0 ..< 100) { num in
+                            Text(String(num) + "時間")
+                                .foregroundColor(.black)
+                        }
                     }
-                }
-                .frame(width: 120, height: 60, alignment: .center)
-                .labelsHidden()
-                .compositingGroup()
-                .clipped()
-                
-            }
-            .padding(.vertical)
-            HStack {
-                Text("鬼の捕獲範囲")
-                    .padding(20)
-                    .foregroundColor(.black)
-                
-                Spacer()
-                
-                Picker("", selection: $killerCaptureRange) {
-                    ForEach(1 ..< 100) { num in
-                        Text(String(num) + "m")
-                            .foregroundColor(.black)
+                    .frame(width: 120, height: 60, alignment: .center)
+                    .labelsHidden()
+                    .compositingGroup()
+                    .clipped()
+                    
+                    Picker("", selection: $minute) {
+                        ForEach(1 ..< 60) { num in
+                            Text(String(num) + "分")
+                                .foregroundColor(.black)
+                        }
                     }
+                    .frame(width: 120, height: 60, alignment: .center)
+                    .labelsHidden()
+                    .compositingGroup()
+                    .clipped()
+                    
                 }
-                .frame(width: 120, height: 60, alignment: .center)
-                .labelsHidden()
-                .compositingGroup()
-                .clipped()
-            }
-            .padding(.vertical)
-            HStack {
-                Text("生存者の位置情報送信間隔")
-                    .padding(20)
-                    .foregroundColor(.black)
-                
-                Spacer()
-                
-                Picker("", selection: $survivorPositionTransmissionInterval) {
-                    ForEach(1 ..< 100) { num in
-                        Text(String(num) + "分")
-                            .foregroundColor(.black)
+                .padding(.vertical)
+                HStack {
+                    Text("逃走時間")
+                        .padding(20)
+                        .foregroundColor(.black)
+                    
+                    Spacer()
+                    Picker("", selection: $escapeTime) {
+                        ForEach(1 ..< 60) { num in
+                            Text(String(num) + "分")
+                                .foregroundColor(.black)
+                        }
                     }
+                    .frame(width: 120, height: 60, alignment: .center)
+                    .labelsHidden()
+                    .compositingGroup()
+                    .clipped()
+                    
                 }
-                .frame(width: 120, height: 60, alignment: .center)
-                .labelsHidden()
-                .compositingGroup()
-                .clipped()
-            }
-            .padding(.vertical)
-            HStack {
-                Text("逃走時間")
-                    .padding(20)
-                    .foregroundColor(.black)
-                
-                Spacer()
-                Picker("", selection: $escapeTime) {
-                    ForEach(1 ..< 60) { num in
-                        Text(String(num) + "分")
-                            .foregroundColor(.black)
+                .padding(.vertical)
+                HStack {
+                    Text("逃走範囲")
+                        .padding(20)
+                        .foregroundColor(.black)
+                    
+                    Spacer()
+                    Picker("", selection: $escapeRange) {
+                        ForEach(1 ..< 500) { num in
+                            Text(String(num * 10) + "m")
+                                .foregroundColor(.black)
+                        }
                     }
+                    .frame(width: 120, height: 60, alignment: .center)
+                    .labelsHidden()
+                    .compositingGroup()
+                    .clipped()
+                    
                 }
-                .frame(width: 120, height: 60, alignment: .center)
-                .labelsHidden()
-                .compositingGroup()
-                .clipped()
-                
-            }
-            .padding(.vertical)
-            HStack {
-                Text("逃走範囲")
-                    .padding(20)
-                    .foregroundColor(.black)
-                
-                Spacer()
-                Picker("", selection: $escapeRange) {
-                    ForEach(1 ..< 500) { num in
-                        Text(String(num * 10) + "m")
-                            .foregroundColor(.black)
+                .padding(.vertical)
+                HStack {
+                    Text("鬼の捕獲範囲")
+                        .padding(20)
+                        .foregroundColor(.black)
+                    
+                    Spacer()
+                    
+                    Picker("", selection: $killerCaptureRange) {
+                        ForEach(1 ..< 100) { num in
+                            Text(String(num) + "m")
+                                .foregroundColor(.black)
+                        }
                     }
+                    .frame(width: 120, height: 60, alignment: .center)
+                    .labelsHidden()
+                    .compositingGroup()
+                    .clipped()
                 }
-                .frame(width: 120, height: 60, alignment: .center)
-                .labelsHidden()
-                .compositingGroup()
-                .clipped()
-                
+                .padding(.vertical)
+                HStack {
+                    Text("生存者の位置情報送信間隔")
+                        .padding(20)
+                        .foregroundColor(.black)
+                    
+                    Spacer()
+                    
+                    Picker("", selection: $survivorPositionTransmissionInterval) {
+                        ForEach(1 ..< 100) { num in
+                            Text(String(num) + "分")
+                                .foregroundColor(.black)
+                        }
+                    }
+                    .frame(width: 120, height: 60, alignment: .center)
+                    .labelsHidden()
+                    .compositingGroup()
+                    .clipped()
+                }
+                .padding(.vertical)
             }
-            .padding(.vertical)
+            .background(Color(red: 0.8, green: 0.8, blue: 0.85))
+            .cornerRadius(10)
         }
-        .background(Color(red: 0.8, green: 0.8, blue: 0.85))
-        .cornerRadius(10)
     }
 }
