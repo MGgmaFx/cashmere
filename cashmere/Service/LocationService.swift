f35b8908c75514c0fe769f9dc08796d261d28ddd
sakamoto <sakamotosusi@gmai.com>
2021-01-26 09:29:32 +0900

    fix: サークルの位置情報取得処理の修正

--- cashmere/Service/LocationService.swift
+++ cashmere/Service/LocationService.swift
@@ -2,38 +2,7 @@
 //  LocationService.swift
 //  cashmere
 //
-//  Created by 織音 on 2021/01/22.
+//  Created by 織音 on 2021/01/26.
 //
 
-
-import SwiftUI
-import UIKit
-import CoreLocation
-
-class requestLocation: NSObject {
-    let manager = CLLocationManager()
-    override init(){
-        super.init()
-        manager.delegate = self
-        manager.desiredAccuracy = kCLLocationAccuracyBest
-        manager.requestWhenInUseAuthorization()
-        manager.requestLocation()
-    }
-}
-
-extension requestLocation: CLLocationManagerDelegate {
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation], completionHandler: @escaping ([String: String]) -> Void) {
-        let location = locations.last
-        let roomLatitude = String(location?.coordinate.latitude ?? 0)
-        let roomLongitude = String(location?.coordinate.longitude ?? 0)
-        let roomLocation = ["roomLatitude": roomLatitude, "roomLongitude": roomLongitude]
-        completionHandler(roomLocation)
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("位置情報の取得に失敗しました")
-    }
-}
-    
-    
+import Foundation
