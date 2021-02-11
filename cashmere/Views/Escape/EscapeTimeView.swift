97c478b7b744ddfb2b985d398bf1f45cbdf6620a
shun-shun <gondo.syun0711@gmail.com>
2021-02-11 14:07:12 +0900

    鬼の待機時間画面を作成(メッセージと色を分けた)

--- cashmere/Views/Escape/EscapeTimeView.swift
+++ cashmere/Views/Escape/EscapeTimeView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct EscapeTimeView: View {
+    @EnvironmentObject var room: Room
     @State var nowDate:Date = Date()
     // 開始時刻
     let startDate: Date
@@ -23,10 +24,14 @@ struct EscapeTimeView: View {
     var body: some View {
         VStack {
             Spacer()
-            Text("逃走時間").foregroundColor(.red).font(.largeTitle)
+            if room.me.role == .killer {
+                Text("待機時間").foregroundColor(.blue).font(.largeTitle)
+            } else {
+                Text("逃走時間").foregroundColor(.red).font(.largeTitle)
+            }
             Spacer()
             
-            TimerFunc(from: startDate)
+            TimerFunc(from: startDate, role: room.me.role)
                 .font(.title)
                 .onAppear(perform: {
                     // timerクロージャの設定
@@ -34,14 +39,18 @@ struct EscapeTimeView: View {
                 })
             
             Spacer()
-            Text("※できるだけ遠くに逃げてください!!").foregroundColor(.white).font(.footnote)
+            if room.me.role == .killer {
+                Text("※待機時間後に逃走者全員を確保してください!!").foregroundColor(.white).font(.footnote)
+            } else {
+                Text("※できるだけ遠くに逃げてください!!").foregroundColor(.white).font(.footnote)
+            }
             Spacer()
         }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center).background(Color.black)
         .edgesIgnoringSafeArea(.all)
     }
     
-    func TimerFunc(from date:Date) -> Text{
+    func TimerFunc(from date:Date, role:Player.Role) -> Text{
         // 現在時刻とゲーム開始時刻の差分を取得
         let cal = Calendar(identifier: .japanese)
         let timeVal = cal.dateComponents([.day,.hour,.minute,.second], from: nowDate,to: startDate)
@@ -52,10 +61,16 @@ struct EscapeTimeView: View {
                 .font(.system(size: 70, weight: .regular, design: .default))
         }
         
+        // 色分け
+        var color = Color.red
+        if role == .killer {
+            color = Color.blue
+        }
+        
         return Text(String(format: "%02d:%02d",
                            timeVal.minute ?? 00,
                            timeVal.second ?? 00))
-            .foregroundColor(.red)
+            .foregroundColor(color)
             .font(.system(size: 100, weight: .regular, design: .default))
         
     }
